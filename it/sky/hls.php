ab1e1e391c0d2a33814d1976c72c289fb2b379cc
Arkina <60531069+Arkina1234@users.noreply.github.com>
2025-07-14 03:40:17 -0400

    Create hls.php

---
+++ it/sky/hls.php
@@ -0,0 +1,9 @@
+<?php
+$id = $_GET['id'];
+
+$json = file_get_contents('https://video.sky.it/api/v1/getLivestream?id='.$id)
+
+$get = json_decode($json);
+
+header('Location: '.$get->streaming_url);
+?>
