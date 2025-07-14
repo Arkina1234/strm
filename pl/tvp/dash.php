63f61fb9e0cc6d79c038d5584443049ad8121c95
Arkina <60531069+Arkina1234@users.noreply.github.com>
2025-07-14 03:38:32 -0400

    Create dash.php

---
+++ pl/tvp/dash.php
@@ -0,0 +1,9 @@
+<?php
+$id = $_GET['id'];
+
+$json = file_get_contents('https://vod.tvp.pl/api/products/'.$id.'/videos/playlist?videoType=LIVE&lang=PL&platform=BROWSER');
+
+$get = json_decode($json);
+
+header('Location: '.$get->sources->DASH[0]->src);
+?>
