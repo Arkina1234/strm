317614f9b823dfb53c03860f4b47ae283a9c64b8
Arkina <60531069+Arkina1234@users.noreply.github.com>
2025-07-14 03:39:28 -0400

    Create hls.php

---
+++ it/rai/hls.php
@@ -0,0 +1,9 @@
+<?php
+$cont = $_GET['cont'];
+
+$json = file_get_contents('https://mediapolis.rai.it/relinker/relinkerServlet.htm?cont='.$cont.'&output=62');
+
+$get = json_decode($json);
+
+header('Location: '.$get->video[0]);
+?>
