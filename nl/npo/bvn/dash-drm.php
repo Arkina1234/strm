5a64012b050dede80282dd41c07d3be969cc9d12
Arkina <60531069+Arkina1234@users.noreply.github.com>
2025-07-30 21:41:09 -0400

    Update dash-drm.php

--- nl/npo/bvn/dash-drm.php
+++ nl/npo/bvn/dash-drm.php
@@ -1,13 +1,13 @@
 <?php
 $context = stream_context_create(
     array(
-        "http" => array(
-            "header" => "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:75.0) Gecko/20100101 Firefox/75.0"
+        'http' => array(
+            'header' => "User-Agent: ' . $_SERVER['HTTP_USER_AGENT']
         )
     )
 );
 
-$token = file_get_contents('https://npo.nl/start/api/domain/player-token?productId=LI_BVN_4589107', false, $context);
+$token = file_get_contents('https://npo.nl/start/api/domain/player-token?productId=LI_BVN_4589107', true, $context);
 $auth = json_decode($token, true);
 
 $ch = curl_init();
