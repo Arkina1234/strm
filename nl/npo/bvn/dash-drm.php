61a5267de545e61d667f4a419ecaed96413915bc
Arkina <60531069+Arkina1234@users.noreply.github.com>
2025-07-30 22:10:23 -0400

    Update dash-drm.php

--- nl/npo/bvn/dash-drm.php
+++ nl/npo/bvn/dash-drm.php
@@ -1,6 +1,18 @@
 <?php
-$token = file_get_contents('https://npo.nl/start/api/domain/player-token?productId=LI_BVN_4589107');
+$ch_token = curl_init();
+curl_setopt($ch_token, CURLOPT_URL, 'https://npo.nl/start/api/domain/player-token?productId=LI_BVN_4589107');
+curl_setopt($ch_token, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch_token, CURLOPT_CUSTOMREQUEST, 'GET');
+curl_setopt($ch_token, CURLOPT_HTTPHEADER, [
+    'User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36',
+    'Content-type: application/json',
+]);
+
+$token = curl_exec($ch_token);
+
 $auth = json_decode($token);
+
+curl_close($ch_token);
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, 'https://prod.npoplayer.nl/stream-link');
